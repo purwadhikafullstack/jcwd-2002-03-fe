25c357c1458f44196868d74efa15da490b264765
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-06-06 15:12:40 +0700

    initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "require-jsdoc": 0,
+    quotes: ["error", "double"],
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+      },
+    ],
+    "arrow-body-style": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "react/prop-types": 0,
+    "react/jsx-props-no-spreading": 0,
+  },
+};
+
