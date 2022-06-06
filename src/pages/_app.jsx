209274e0ac1d6366be70e9e896a340a84a075954
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-06-06 21:45:10 +0700

    component: added navbar

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,13 @@
-import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react"
+import NavBar from "./component/NavBar"
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+  return (
+    <ChakraProvider>
+      <NavBar />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
 
-export default MyApp;
-
+export default MyApp
