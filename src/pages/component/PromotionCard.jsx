668805cb87f46caed11d4697fef485127d524ce5
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-06-07 01:24:54 +0700

    component: added bottom banner

--- src/pages/component/PromotionCard.jsx
+++ src/pages/component/PromotionCard.jsx
@@ -3,14 +3,15 @@ import { Box, Img, SimpleGrid } from "@chakra-ui/react";
 
 const PromotionCard = () => {
     return (
-        <SimpleGrid columns={[1, 2]} spacing="5px" alignItems="center" justifyContent="center">
-            <Box height={["124px", "212px", "212px"]} margin={0}>
-                <Img width="100%" height={["124px", "212px", "212px"]} src="programHamil.svg" />
+        <SimpleGrid columns={[1, 3]} spacing={5} alignItems="center" justifyContent="center">
+            <Box height="auto" margin={0}>
+                <Img width="100%" src="programHamil.svg" />
             </Box>
-            <Box height={["124px", "212px", "212px"]} margin={0}>
-                <Img width="100%" height={["124px", "212px", "212px"]} src="idulFitri.svg" />
+            <Box height="auto" margin={0}>
+                <Img width="100%" src="idulFitri.svg" />
             </Box>
         </SimpleGrid>
+
     )
 }
 
