8d6f99353cd85cf32fd2bfa5b4eba081187154f3
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-06-07 23:27:38 +0700

    added prduct detail card

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,7 @@ import BottomBanner from "../component/BotomBanner"
 import PromotionCard from "../component/PromotionCard"
 import ResepBanner from "../component/ResepBanner"
 import SlideShow from "../component/SlideShow"
+import ProdukDetail from "../component/ProdukDetail"
 
 const Home = () => {
     return (
