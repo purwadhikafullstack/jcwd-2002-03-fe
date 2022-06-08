5ca9acae4ce43d8dafdbe6ac94df18b73e4ed7f3
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-06-08 13:52:29 +0700

    page:product detail added, need to fix slide show comp

--- src/component/PromotionCard.jsx
+++ src/component/PromotionCard.jsx
@@ -5,10 +5,10 @@ const PromotionCard = () => {
     return (
         <SimpleGrid columns={[1, 2, 2]} spacing={5} alignItems="center" justifyContent="center">
             <Box height="auto" margin={0}>
-                <Img width="100%" src="programHamil.svg" />
+                <Img width="100%" src="/programHamil.svg" />
             </Box>
             <Box height="auto" margin={0}>
-                <Img width="100%" src="idulFitri.svg" />
+                <Img width="100%" src="/idulFitri.svg" />
             </Box>
         </SimpleGrid>
     )
