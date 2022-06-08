5abf5a423b77c102fb1876cfe639ab290df748d7
akimajid <akimajid07@gmail.com>
2022-06-08 09:30:05 +0700

    feat: added admin login page (#3)

---
+++ src/component/LeftSectionAdminLogin.jsx
@@ -0,0 +1,11 @@
+import { Heading, Stack, Img, Container, Box } from "@chakra-ui/react"
+
+const LeftSectionAdminLogin = () => {
+    return (
+        <Stack>
+            <Img src="loginAdminLogo.svg" />
+        </Stack>
+    )
+}
+
+export default LeftSectionAdminLogin
---
+++ src/pages/admin-login.jsx
@@ -0,0 +1,19 @@
+import { Center, Container, SimpleGrid } from "@chakra-ui/react"
+import LeftSectionAdminLogin from "component/LeftSectionAdminLogin"
+import RightSectionAdminLogin from "component/RightSectionAdminLogin"
+
+const AdminLogin = () => {
+    return (
+        <Container
+        height="full"
+        as={SimpleGrid}
+        maxW="full"
+        columns={{ base: 2, md: 2 }}
+        spacing={{ base: 1, md: 2 }}>
+            <LeftSectionAdminLogin />
+            <RightSectionAdminLogin />
+        </Container>
+    )
+}
+
+export default AdminLogin
