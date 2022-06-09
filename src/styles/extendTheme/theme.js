812839ff2e5be9c826f189e2c3a4dd08ba994dbe
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-06-09 23:00:56 +0700

    style: added extend theme

---
+++ src/styles/extendTheme/theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react"
+import Text from "./text"
+import Button from "./button"
+
+const theme = extendTheme({
+    components: {
+        Text,
+        Button
+    }
+})
+
+export default theme
