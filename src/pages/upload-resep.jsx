fde900799794e3388946d1417333cbe4005d64d0
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-06-12 00:02:31 +0700

    page:drop and drag added

--- src/pages/upload-resep.jsx
+++ src/pages/upload-resep.jsx
@@ -1,40 +1,81 @@
-import React, { useRef } from "react"
-import { Box, Button, Container, Divider, Input, Text } from "@chakra-ui/react"
+import React, { useEffect, useRef, useState } from "react"
+import { Box, Button, Container, Divider, Grid, GridItem, Input, List, ListIcon, ListItem, Text } from "@chakra-ui/react"
+import { useDropzone } from "react-dropzone";
+import { MdCheckCircle } from "react-icons/md"
 
 const uploadResep = () => {
     const inputFileRef = useRef()
+    const [show, setShow] = useState(false)
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+        accept: {
+            "image/jpeg": [".png", ".jpg", ".jpeg"],
+        },
+        noClick: true,
+        noKeyboard: true
+    });
+
+    const trigerShow = () => {
+        if (acceptedFiles.length) {
+            setShow(true)
+        }
+    }
+
+    const files = acceptedFiles.map((file) => (
+        <List >
+            <ListItem key={file.path}>
+                <ListIcon as={MdCheckCircle} color='green.500' />
+                {file.path} - {file.size} bytes
+            </ListItem>
+        </List>
+    ));
+
+    useEffect(() => {
+        trigerShow()
+    }, [acceptedFiles])
+
     return (
         <Container
-            width="90%"
-            maxWidth="90%"
-            marginLeft="auto"
-            marginRight="auto"
+            width={["100%", "90%", "90%"]}
+            maxWidth={["100%", "90%", "90%"]}
+            marginLeft={["0px", "auto", "auto"]}
+            marginRight={["0px", "auto", "auto"]}
+            marginBottom={[10, 10, 10]}
         >
-            <Text fontWeight={700} fontSize={[20, 24, 24]}>Kirim Resep</Text>
+            <Text display={["none", "flex", "flex"]} variant="title">Kirim Resep</Text>
             <Text
-                fontWeight={400}
-                fontSize={[12, 14, 14]}
+                variant="caption"
+                display={["none", "flex", "flex"]}
                 marginBottom={[3, 5, 5]}
             >
                 Tak perlu antre &amp; obat langsung dikirimkan ke lokasi anda! <b>Foto tidak boleh lebih dari 10 MB</b>
             </Text>
+
             <Box
                 // display="flex"
                 justifyContent="center"
                 alignItems="center"
-                width="70%"
-                height="70vh"
-                marginLeft="auto"
-                marginRight="auto"
-                boxShadow="0px 2px 3px 2px rgba(33, 51, 96, 0.02), 0px 4px 12px 4px rgba(0, 155, 144, 0.08)"
+                width={["100%", "70%", "70%"]}
+                height={["50vh", "70vh", "70vh"]}
+                marginLeft={["0px", "auto", "auto"]}
+                marginRight={["0px", "auto", "auto"]}
+                boxShadow={["none", "0px 2px 3px 2px rgba(33, 51, 96, 0.02), 0px 4px 12px 4px rgba(0, 155, 144, 0.08)"]}
                 padding={[3, 6, 6]}
                 borderRadius="18px"
-                marginBottom={[5, 10, 10]}
+                marginBottom={[10, 10, 10]}
             >
-                <Text fontWeight={600} fontSize={[12, 14, 14]}>
+                <Text variant={["title", "caption-bold"]}>
                     Unggah Resep Dokter
                 </Text>
                 <Divider mb={[6, 8, 8]} mt={[2, 3, 3]} />
+                <Box display={["block", "none", "none"]}>
+                    <Text
+                        variant="caption"
+                        marginBottom={[3, 5, 5]}
+                    >
+                        Tak perlu antre &amp; obat langsung dikirimkan ke lokasi anda! <b>Foto tidak boleh lebih dari 10 MB</b>
+                    </Text>
+                </Box>
+
                 <Box
                     border="1px dashed teal"
                     borderRadius="16px"
@@ -43,39 +84,44 @@ const uploadResep = () => {
                     width="100%"
                     height="75%"
                     alignItems="center"
-                    marginLeft="auto"
-                    marginRight="auto"
                     background="#F6FAFB"
+                    {...getRootProps({ className: "dropzone" })}
                 >
-                    <Box
-                        display="flex"
-                        justifyContent="center"
-                        alignItems="center"
-                        marginLeft="auto"
-                        marginRight="auto"
-                    >
-                        <Input type="file" accept="image/png, image/jpeg" ref={inputFileRef} display="none" />
-                        <Button onClick={() => inputFileRef.current.click()} colorScheme="teal">
+
+                    <Box>
+                        <Text textAlign="center" display={["none", "block", "block"]}>Tarik &amp; Letakan File </Text>
+                        <Text textAlign="center" display={["none", "block", "block"]}>atau </Text>
+                        <Input {...getInputProps()} ref={inputFileRef} />
+                        <Button onClick={() => inputFileRef.current.click()} variant="main">
                             Unggah Resep
                         </Button>
                     </Box>
                 </Box>
-                <Box
+
+
+                <Grid
+                    templateColumns={["repeat(1, 1fr)", "repeat(3, 1fr)"]} gap={2}
                     mt={[3, 6, 6]}
-                    mb={[6, 10, 10]}
-                    display="flex"
-                    justifyContent="right"
-                    alignItems="center"
+                    mb={[10, 10, 10]}
                 >
-                    <Button marginRight={[3, 6, 6]} variant="outline" colorScheme="teal">
-                        Cancel
-                    </Button>
-                    <Button marginRight={[3, 6, 6]} variant="solid" colorScheme="teal">
-                        Unggah
-                    </Button>
-                </Box>
+                    <GridItem justifyContent="space-between" display="flex" alignItems="center" colSpan={[1, 2, 2]}>{files}</GridItem>
+                    <GridItem colSpan={[1, 1, 1]} justifyContent="space-evenly" display="flex" alignItems="center">
+                        {show ?
+                            <>
+                                <Button variant="main-outline">
+                                    Cancel
+                                </Button>
+                                <Button variant="main">
+                                    Unggah
+                                </Button>
+                            </>
+                            : ""
+                        }
+                    </GridItem>
+
+                </Grid>
             </Box>
-        </Container>
+        </Container >
     )
 }
 
--- src/styles/extendTheme/text.js
+++ src/styles/extendTheme/text.js
@@ -56,7 +56,7 @@ const Text = {
             }
         },
         "mini-title": {
-            fontWeight: 700,
+            fontWeight: 600,
             color: "#213360",
             fontSize: {
                 base: "12px",
