be8a1af70e20829101d9b504c13cae4f263dfaa8
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-06-13 23:00:41 +0700

    component: dynamic bottom navbar modified

--- src/component/bottomNavBar/HomeBottomNavBar.jsx
+++ src/component/bottomNavBar/HomeBottomNavBar.jsx
@@ -5,11 +5,13 @@ import { BiCategoryAlt } from "react-icons/bi"
 import { RiBillLine } from "react-icons/ri"
 import { TbHeadset } from "react-icons/tb"
 import { FaRegUserCircle } from "react-icons/fa"
+import Link from "next/link"
 
 const HomeBottomNavBar = () => {
     return (
 
         <Grid
+            // color="#F6FAFB"
             background="#F6FAFB"
             templateColumns="repeat(5, 1fr)"
             display={["flex", "none", "none"]}
@@ -22,26 +24,37 @@ const HomeBottomNavBar = () => {
             right={0}
             position="fixed"
             mt={10}
+        // alignItems="center"
 
         >
             <GridItem colSpan={1} alignItems="center" display="block" justifyContent="center" textAlign="center">
-                <Icon as={HiHome} boxSize="35px" color="#B4B9C7" />
+                <Link href="/">
+                    <Icon as={HiHome} boxSize="35px" color="#B4B9C7" />
+                </Link>
                 <Text>Beranda</Text>
             </GridItem>
             <GridItem colSpan={1} alignItems="center" display="block" justifyContent="center" textAlign="center">
-                <Icon as={BiCategoryAlt} boxSize="35px" color="#B4B9C7" />
+                <Link href="/product-list">
+                    <Icon as={BiCategoryAlt} boxSize="35px" color="#B4B9C7" />
+                </Link>
                 <Text>Kategori</Text>
             </GridItem>
             <GridItem colSpan={1} alignItems="center" display="block" justifyContent="center" textAlign="center">
-                <Icon as={RiBillLine} boxSize="35px" color="#B4B9C7" />
+                <Link href="/cart">
+                    <Icon as={RiBillLine} boxSize="35px" color="#B4B9C7" />
+                </Link>
                 <Text>Transaksi</Text>
             </GridItem>
             <GridItem colSpan={1} alignItems="center" display="block" justifyContent="center" textAlign="center">
-                <Icon as={TbHeadset} boxSize="35px" color="#B4B9C7" />
+                <Link href="/help">
+                    <Icon as={TbHeadset} boxSize="35px" color="#B4B9C7" />
+                </Link>
                 <Text>Bantuan</Text>
             </GridItem>
             <GridItem colSpan={1} alignItems="center" display="block" justifyContent="center" textAlign="center">
-                <Icon as={FaRegUserCircle} boxSize="35px" color="#B4B9C7" />
+                <Link href="/profile">
+                    <Icon as={FaRegUserCircle} boxSize="35px" color="#B4B9C7" />
+                </Link>
                 <Text>Profile</Text>
             </GridItem>
         </Grid >
