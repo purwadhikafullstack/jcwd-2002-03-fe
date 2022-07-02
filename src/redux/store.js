73b2dad7885bc39f3708d60c05da5bebc229e72c
akimajid <akimajid07@gmail.com>
2022-07-02 21:19:20 +0700

    feat: added filter name and/or category

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducer/authSlice";
 import profileReducer from "./reducer/profileSlice";
+import searchReducer from "./reducer/search";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     profile: profileReducer,
+    search: searchReducer
   },
 });
 
