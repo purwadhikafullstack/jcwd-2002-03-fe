402dac9ddae915e06ee9d00954ed9bf65fa66249
Arlonas <98306152+Arlonas@users.noreply.github.com>
2022-07-08 13:27:41 +0700

    feat: added cart (#46)

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducer/authSlice";
 import profileReducer from "./reducer/profileSlice";
 import searchReducer from "./reducer/search";
+import cartReducer from "./reducer/cartSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     profile: profileReducer,
-    search: searchReducer
+    search: searchReducer,
+    cart: cartReducer,
   },
 });
 
-export default store
-
+export default store;
