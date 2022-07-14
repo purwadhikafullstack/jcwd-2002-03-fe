d1a0d33057046c6f35c056254c0f9d2018ea6b26
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-07-14 17:29:55 +0700

    Feat/[P3-30][P3-34]

--- src/component/transaction/TransactionCard.jsx
+++ src/component/transaction/TransactionCard.jsx
@@ -1,29 +1,32 @@
-import { Badge, Box, Button, Checkbox, Divider, HStack, Icon, Text } from "@chakra-ui/react"
+import { Box, Button, Checkbox, Divider, HStack, Icon, Stack, Text } from "@chakra-ui/react"
 import React from "react"
-import { FaReceipt } from "react-icons/fa"
 import { IoChatbubbleEllipses } from "react-icons/io5"
 import moment from "moment"
 import "moment/locale/id"
+import { TbClock } from "react-icons/tb"
 import { MdArrowDropDown } from "react-icons/md"
 import PrescriptionsHandler from "./PrescriptionsHandler"
+import TransactionDetail from "./TransactionDetail"
 
 const TransactionCard = ({ props, fetchTransaction }) => {
-
     console.log(props)
+
     return (
         <Box mt={4} bgColor="whiteAlpha.800" padding={4} width="100%" boxShadow="0px 2px 3px 2px rgba(33, 51, 96, 0.02), 0px 4px 12px 4px rgba(0, 155, 144, 0.08)">
-            <Box borderBottom="1px solid black" display="flex" justifyContent="space-between">
+            <Box borderBottom="1px solid black" display="flex" justifyContent="space-between" paddingBottom={2}>
                 <Box>
                     <HStack spacing="20px">
                         <Checkbox>Pesanan Selesai</Checkbox>
                         <Text>{props.nomer_pesanan}</Text>
-                        <Text>{moment(props.createdAt).format("LLL")}</Text>
+                        <Text alignItems="center">{moment(props.createdAt).format("LLL")}</Text>
                     </HStack>
                 </Box>
                 <Box>
                     <HStack spacing="10px">
                         <Text>Response Sebelum</Text>
-                        <Badge>{moment(props.createdAt).add(2, "days").calendar()}</Badge>
+                        <Box alignItems="center" display="flex" paddingX={2} background="rgba(255, 222, 107, 0.3)">
+                            <Icon as={TbClock} mr={1} />{moment(props.createdAt).add(2, "days").format("LLL")}
+                        </Box>
                     </HStack>
                 </Box>
             </Box>
@@ -68,50 +71,53 @@ const TransactionCard = ({ props, fetchTransaction }) => {
                         </>
                     }
                 </Box>
-                <Box height="inherit">
+                <Stack direction="row" height="inherit" paddingY={4}>
                     <Divider orientation="vertical" />
-                </Box>
-                <Box>
-                    <Text>Pembeli</Text>
+                </Stack>
+                <Box ml="-50px">
+                    <Text><b>Pembeli</b></Text>
                     <Text>{props.User.name}</Text>
                 </Box>
                 <Box>
-                    <Text>Alamat</Text>
+                    <Text><b>Alamat</b></Text>
                     <Text>{props.Address.alamat}</Text>
                     <Text>{props.Address.kecamatan}, {props.Address.kotaKabupaten}</Text>
                     <Text>{props.Address.provinsi} - {props.Address.kodePos}</Text>
                 </Box>
                 <Box>
-                    <Text>Kurir</Text>
+                    <Text><b>Kurir</b></Text>
                     <Text>Name Kurir</Text>
                 </Box>
                 <Box />
             </Box>
-            <Box alignItems="center" background="#F6FAFB" borderRadius="4px" display="flex" justifyContent="space-between" padding={3}>
+            <Box height="40px" alignItems="center" background="#F6FAFB" borderRadius="4px" display="flex" justifyContent="space-between" padding={3}>
                 {props.Transaction_items.length !== 0 &&
                     <>
                         <Box>
-                            <Text>Total Harga ({props.Transaction_items.length} obat)</Text>
+                            <Text><b>Total Harga</b> ({props.Transaction_items.length} obat)</Text>
                         </Box>
-                        <Box>Rp.{props.total_price.toLocaleString()}</Box>
+                        <Box><b>Rp.{props.total_price.toLocaleString()}</b></Box>
                     </>
                 }
             </Box>
             <Box alignItems="center" display="flex" justifyContent="space-between" padding={2}>
                 <HStack spacing={5}>
-                    <Box alignItems="center">
-                        <Icon as={IoChatbubbleEllipses} mr={1} />
-                        Chat Pembeli
-                    </Box>
-                    <Box alignItems="center">
-                        <Icon as={FaReceipt} mr={1} />
-                        Detail Pemesanan
-                    </Box>
+                    <Button variant="ghost" colorScheme="teal"><Icon as={IoChatbubbleEllipses} mr={1} />
+                        Chat Pembeli</Button>
+                    <TransactionDetail
+                        key={props.id}
+                        transactionDetail={props.Transaction_items}
+                        nomerPesanan={props.nomer_pesanan}
+                        username={props.User.name}
+                        dateOrder={moment(props.createdAt).format("LLL")}
+                        totalPrice={props.total_price.toLocaleString()}
+                        itemsLength={props.Transaction_items.length}
+                    />
                 </HStack>
                 <Box display="flex" justifyContent="space-between" alignItems="center">
                     <HStack spacing={5}>
-                        <Text>Tolak Pesanan</Text>
-                        <Button>Terima Pesanan</Button>
+                        <Button variant="ghost" colorScheme="teal">Tolak Pesanan</Button>
+                        <Button colorScheme="teal">Terima Pesanan</Button>
                     </HStack>
                 </Box>
             </Box>
